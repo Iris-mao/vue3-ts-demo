2f5b89cf1448c530fe2e626e391f2c1fc3693af2
Iirs-mao <527915923@qq.com>
2020-03-17 10:47:56 +0800

    vue-class-component 语法初试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,65 @@
+<!--<template>-->
+<!--  <div class="home">-->
+<!--    <div class="content">-->
+<!--      <div class="home-header">-->
+<!--        <router-link class="link" to="/home"></router-link>-->
+<!--        <img class="home-logo" src="../assets/logo.png" alt="logo">-->
+<!--      </div>-->
+<!--      <div class="home-body">-->
+<!--        <div class="list">-->
+<!--          <router-link class="link" to="/articles">文章</router-link>-->
+<!--          <router-link class="link" to="/project">项目</router-link>-->
+<!--          <a target="_blank" rel="noopener noreferrer" class="link" href="https://github.com/biaochenxuying">github</a>-->
+<!--        </div>-->
+<!--        <div class="introduce">顺其自然</div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--// @ is an alias to /src-->
+<!--// import HelloWorld from "@/components/HelloWorld.vue";-->
+
+<!--export default {-->
+<!--  name: "Home",-->
+<!--  components: {-->
+<!--    // HelloWorld-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
+
 <template>
-  <div class="home">
-    <div class="content">
-      <div class="home-header">
-        <router-link class="link" to="/home"></router-link>
-        <img class="home-logo" src="../assets/logo.png" alt="logo">
-      </div>
-      <div class="home-body">
-        <div class="list">
-          <router-link class="link" to="/articles">文章</router-link>
-          <router-link class="link" to="/project">项目</router-link>
-          <a target="_blank" rel="noopener noreferrer" class="link" href="https://github.com/biaochenxuying">github</a>
-        </div>
-        <div class="introduce">顺其自然</div>
-      </div>
-    </div>
+  <div>
+    <input v-model="msg" />
+    <p>msg:{{ msg }}</p>
+    <p>computed msg: {{ computedMsg }}</p>
+    <button @click="greet">Greet</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
 
-export default {
-  name: "Home",
-  components: {
-    // HelloWorld
+@Component
+export default class App extends Vue {
+  // 初始化数据
+  msg = 123;
+
+  // 声明周期钩子
+  mounted() {
+    this.greet();
   }
-};
+
+  // 计算属性
+  get computedMsg() {
+    return "computed" + this.msg;
+  }
+
+  // 方法
+  greet() {
+    alert("greeting:" + this.msg);
+  }
+}
 </script>
